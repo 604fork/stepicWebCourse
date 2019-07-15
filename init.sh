9dc2219126979758f7e3476a07ff0c33d83211bf
Vladislav Volkov <vladislav.volkov@gs-labs.ru>
2019-07-15 17:54:24 +0300

    Changed init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/stepic.nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
