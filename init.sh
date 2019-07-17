ae22d09b8c5f02a03b094f819beacf7657b17da8
604_fork <604_fork@mail.ru>
2019-07-17 12:01:19 +0000

    Changed an init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
+git config --global user.email "604_fork@mail.ru"                              
+git config --global user.name "604_fork"
 sudo unlink /etc/nginx/sites-enabled/default
 sudo unlink /etc/nginx/sites-enabled/test.conf
+sudo unlink /etc/gunicorn.d/hello.py 
+sudo ln -s /home/box/web/etc/gunicornConfig.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/stepic.nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
